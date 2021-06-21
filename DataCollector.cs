fd20373fb6545106eb7543bc512b0bb735268c0f
MT250 <40330325+MT250@users.noreply.github.com>
2021-06-21 13:51:17 +0300

    Initial commit

---
+++ DataCollector.cs
@@ -0,0 +1,33 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Linq;
+
+namespace AzureFunctionApp
+{
+    public static class DataCollector
+    {
+        [FunctionName("DataCollector")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+
+
+            Console.WriteLine(requestBody);          
+
+            return new OkObjectResult(null);
+        }
+    }
+}
